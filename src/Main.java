ccdc338dc81f0225657d125495813e2f77fe1c43
Алла Демидова <Alla.demidova.it@gmail.com>
2025-11-04 19:02:27 +0300

    В Main добавлена логика арифметических действий с числами

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
 import java.util.Scanner;
-
+//
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите первое число:");
+        int number1 = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int number2 = new Scanner(System.in).nextInt();
+        int add = number1 + number2;
+        int sub = number1 - number2;
+        int mult = number1 * number2;
+        double div = (double) number1 / number2;
+        System.out.println("Сумма чисел: " + add);
+        System.out.println("Разность чисел: " + sub);
+        System.out.println("Произведение чисел: " + mult);
+        System.out.println("Деление чисел: " + div);
     }
 }
